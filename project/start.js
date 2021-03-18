8d1405a2030d905f5e2daf2ed28df7b77b18ccbc
Chaim Weinerman <chaimw@hotmail.ca>
2021-03-17 20:30:32 -0400

    cron time change

--- project/start.js
+++ project/start.js
@@ -36,7 +36,7 @@ app.listen(app.get('port'), function () {
 
 const cron = require('node-cron');
 
-const task = cron.schedule('45 20 * * *', () => {
+const task = cron.schedule('35 20 * * *', () => {
   console.log('running...');
 
   var firebaseConfig = {
